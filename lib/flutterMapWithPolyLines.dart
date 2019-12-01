702cb6f6dcd5c7cde320ca40bcf805e96415ae4a
fonkamloic <fonkamloic@gmail.com>
2019-12-01 02:11:21 +0100

    flutter map with polylines

---
+++ lib/flutterMapWithPolyLines.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<LatLng> points;
+  MapController mapController;
+
+  List<Marker> markers;
+
+  @override
+  void initState() {
+    mapController = MapController();
+    points = List<LatLng>();
+    points.add(LatLng(41.8781, -87.6298));
+    points.add(LatLng(42.3314, -83.0458));
+    points.add(LatLng(42.7325, -84.5555));
+
+    markers = List<Marker>();
+
+    for (int i = 0; i < points.length; i++) {
+      markers.add(Marker(
+        width: 80.0,
+        height: 80.0,
+        point: points.elementAt(i),
+        builder: (ctx) => Icon(Icons.pin_drop, color: Colors.red),
+      ));
+    }
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Name here"),
+        ),
+        body: Container(
+            padding: EdgeInsets.all(32.0),
+            child: Column(
+              children: <Widget>[
+                Flexible(
+                  child: FlutterMap(
+                    mapController: mapController,
+                    options: MapOptions(
+                        center: LatLng(41.8781, -87.6298), zoom: 5.0),
+                    layers: [
+                      TileLayerOptions(
+                          urlTemplate:
+                              "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                          subdomains: ['a', 'b', 'c']),
+                      MarkerLayerOptions(markers: markers),
+                      PolylineLayerOptions(polylines: [
+                        Polyline(
+                          points: points,
+                          strokeWidth: 4.0,
+                          color: Colors.purple,
+                        ),
+                      ]),
+                    ],
+                  ),
+                )
+              ],
+            )),
+      ),
+    );
+  }
+}
