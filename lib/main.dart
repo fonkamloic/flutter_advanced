7f02f256aed186e46741889e99cc0ad69a87853a
fonkamloic <fonkamloic@gmail.com>
2019-12-31 17:36:16 +0100

    injected dependencies before runApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,73 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_3d_obj/flutter_3d_obj.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  // This widget is the root of your application.
-  @override
-  _MyAppState createState() => _MyAppState();
+void main() {
+  runApp(new MyApp());
 }
 
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  MyHomePageState createState() => new MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
-          title: Text("Name here"),
+          title: Text('3D Touchable Model'),
         ),
-        body: Container(
-            padding: EdgeInsets.all(32.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[],
-            )),
-      ),
+        body: Touch3DModel());
+  }
+}
+
+class Touch3DModel extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return Touch3DModelState();
+  }
+}
+
+class Touch3DModelState extends State<Touch3DModel> {
+  double x_coord = 100.0;
+  double y_coord = 100.0;
+
+  void onTapDown(BuildContext context, TapDownDetails details) {
+    print('${details.globalPosition}');
+    final RenderBox box = context.findRenderObject();
+    final Offset localOffset = box.globalToLocal(details.globalPosition);
+    setState(() {
+      x_coord = localOffset.dx;
+      y_coord = localOffset.dy;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: (TapDownDetails details) => onTapDown(context, details),
+      child: Stack(fit: StackFit.expand, children: <Widget>[
+        Container(color: Colors.grey),
+        Positioned(
+          child: Object3D(
+            size: const Size(200.0, 200.0),
+            path: "assets/file.obj",
+            asset: true,
+          ),
+          left: x_coord,
+          top: y_coord,
+        )
+      ]),
     );
   }
 }
